a72ad80e2d5497ebc7bc6eda6367c8c734e85977
shin3030 <jim1425366@gmail.com>
2022-09-28 16:00:16 +0800

    first commit_0919

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   var appTitle = Text('my first flutter app'),
       hiFlutter = Text(
-        'Hi,Flutter \n 今天是20229/26',
+        'Hi,Flutter \n 今天是20229/28',
         style: TextStyle(fontSize:30,color:Colors.red,
         decoration: TextDecoration.underline,),
 
       );
   var ing=Image.network("https://memeprod.ap-south-1.linodeobjects.com/user-gif-post/1645130473187.gif");
-  var appBody = Center(child:ing,);
-  var appBar = AppBar(title:appTitle,
-    backgroundColor: Colors.grey[400],);
+  var appBar = AppBar(title:appTitle,backgroundColor: Colors.grey[400],);
+  var appBody= Container(
+    child:hiFlutter,
+    color: Colors.grey[900],
+    alignment: Alignment.topCenter,
+    margin: EdgeInsets.all(50),
+    padding: EdgeInsets.fromLTRB(30,0 ,30,30),
+  );
   var app = MaterialApp(
     home:Scaffold(
       appBar:appBar,
@@ -19,5 +24,6 @@ void main() {
       backgroundColor: Colors.grey[800],
     ),
   );
+
   runApp(app);
 }
