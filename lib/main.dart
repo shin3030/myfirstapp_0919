cdbced98aaca7813682e236eeddabf1dff3b1db6
shin3030 <jim1425366@gmail.com>
2022-10-03 15:48:59 +0800

    first commit_0919

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,51 @@ void main() {
 
       );
   var ing=Image.network("https://memeprod.ap-south-1.linodeobjects.com/user-gif-post/1645130473187.gif");
+  var ing2=Image.network("https://memeprod.ap-south-1.linodeobjects.com/user-gif-post/1635824885617.gif");
   var appBar = AppBar(title:appTitle,backgroundColor: Colors.grey[400],);
-  var appBody= Container(
-    child:hiFlutter,
-    color: Colors.grey[900],
-    alignment: Alignment.topCenter,
-    margin: EdgeInsets.all(50),
-    padding: EdgeInsets.fromLTRB(30,0 ,30,30),
+  var appBody= GridView.count(
+    primary: false,
+    padding: const EdgeInsets.all(20),
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+    crossAxisCount: 2,
+    children: <Widget>[
+      Container(
+        padding: const EdgeInsets.all(8),
+        child:new Image.network("https://memeprod.ap-south-1.linodeobjects.com/user-gif-post/1635824885617.gif",
+        fit: BoxFit.fill,),
+
+        color: Colors.grey[100],
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        child: const Text('OAO') ,
+        color: Colors.grey[200],
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        child: const Text('@_@'),
+        color: Colors.grey[300],
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        child:ing2,
+        color: Colors.grey[400],
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        child: const Text('/3\\'),
+        color: Colors.grey[500],
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        child: const Text('冏'),
+        color: Colors.grey[600],
+      ),
+    ],
   );
+
+
   var app = MaterialApp(
     home:Scaffold(
       appBar:appBar,
